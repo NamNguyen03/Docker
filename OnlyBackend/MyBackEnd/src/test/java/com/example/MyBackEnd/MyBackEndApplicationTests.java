43d36d4706e9f6b40810b8a51fc079056d88c91f
NamNguyen03 <75822080+NamNguyen03@users.noreply.github.com>
2021-10-20 10:20:36 +0700

    add hello world

---
+++ OnlyBackend/MyBackEnd/src/test/java/com/example/MyBackEnd/MyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
